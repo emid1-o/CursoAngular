b6f1334c4a81526e33511c21ecd42fdd51faf606
emid1-o <emidiolop5@gmail.com>
2025-07-26 16:05:15 -0300

    exemplo componentes

--- Secao4/fundamentos angular  componentes/compoAula/src/app/app.module.ts
+++ Secao4/fundamentos angular  componentes/compoAula/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Comp2Component } from './comp2/comp2.component';
+import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp2Component
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
   ],
   imports: [
     BrowserModule,
